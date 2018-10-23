5f808c8eea0ec88546c230fb5a2795ccecd4b1dc
GeoDaoyu <geodaoyu@foxmail.com>
2018-10-23 21:00:31 +0800

    import koa2-cors

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const Koa = require('koa')
 const controller = require('./controller')
 const cfg = require('./config')
+const cors = require('koa2-cors')
 const app = new Koa()
 
-const koaBody = require('koa-body');
+const koaBody = require('koa-body')
 
 app.use(koaBody({
   multipart: true,
   formidable: {
     maxFileSize: 200*1024*1024
   }
-}));
+}))
+
+app.use(cors({
+  origin: function (ctx) {
+      return 'http://localhost:8081'
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+
 // app.use(bodyParser())
 app.use(controller())
 

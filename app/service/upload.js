dd503e2216030f7ac75da498560d6634d107b19f
GeoDaoyu <geodaoyu@foxmail.com>
2018-10-25 09:40:01 +0800

    upload 中文名解码

--- app/service/upload.js
+++ app/service/upload.js
@@ -1,16 +1,14 @@
 const path = require('path')
 const fs = require('fs')
-
+const cfg = require('../../config/config.default')
 
 module.exports = {
   async impl (ctx) {
     const filePaths = [];
     const files = ctx.request.files || {};
-    console.log(ctx.path)
-    const foler = ctx.path
     for (let key in files) {
       const file = files[key];
-      const filePath = path.join(foler, file.name);
+      const filePath = path.join(cfg.root, decodeURI(ctx.path));
       const reader = fs.createReadStream(file.path);
       const writer = fs.createWriteStream(filePath);
       reader.pipe(writer);

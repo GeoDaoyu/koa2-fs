3e7ed8c342db0a0c27dd5d3b507ea11816bb07ae
zhanggy <geodaoyu@foxmail.com>
2018-11-01 17:36:48 +0800

    console.info err

--- app/helper/error.js
+++ app/helper/error.js
@@ -1,7 +1,9 @@
+const chalk = require('chalk')
 module.exports = async (ctx, next) => {
   try {
     await next();
   } catch (err) {
+    console.info(`${chalk.red(err.message)}`)
     ctx.response.status = err.statusCode || err.status || 500
     ctx.response.body = {
       error: {
